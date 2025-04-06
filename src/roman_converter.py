b665eef011698a6744297384b22e540396933c8a
TtheNix <nahuelfacundox@gmail.com>
2025-04-06 10:59:59 -0300

    Feat:Logica de los test para que los test pasen

--- src/roman_converter.py
+++ src/roman_converter.py
@@ -1,24 +1,13 @@
-import unittest
-from src.roman_converter import decimal_to_roman
+def decimal_to_roman(numero):
+    valores = [
+        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+        (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+        (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+    ]
 
-class TestRomanConverter(unittest.TestCase):
-    def test_basic_numbers(self):
-        self.assertEqual(decimal_to_roman(1), "I")
-        self.assertEqual(decimal_to_roman(5), "V")
-        self.assertEqual(decimal_to_roman(10), "X")
-
-    def test_subtraction_rules(self):
-        self.assertEqual(decimal_to_roman(4), "IV")
-        self.assertEqual(decimal_to_roman(9), "IX")
-        self.assertEqual(decimal_to_roman(40), "XL")
-        self.assertEqual(decimal_to_roman(90), "XC")
-
-    def test_complex_numbers(self):
-        self.assertEqual(decimal_to_roman(49), "XLIX")
-        self.assertEqual(decimal_to_roman(99), "XCIX")
-        self.assertEqual(decimal_to_roman(499), "CDXCIX")
-        self.assertEqual(decimal_to_roman(999), "CMXCIX")
-        self.assertEqual(decimal_to_roman(3999), "MMMCMXCIX")
-
-if __name__ == '__main__':
-    unittest.main()
+    resultado = ''
+    for valor, simbolo in valores:
+        while numero >= valor:
+            resultado += simbolo
+            numero -= valor
+    return resultado
